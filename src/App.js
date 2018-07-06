dcc72b31df0f818145654cc7d10e921af404e87c
Serg <serg1978ch@ukr.net>
2018-07-06 20:05:53 +0300

    First commit

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+import Grid from 'react-bootstrap/lib/Grid';
+import Row from 'react-bootstrap/lib/Row';
+import Col from 'react-bootstrap/lib/Col';
+import styled from 'styled-components';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="wrapper">
+        <SayFulName name="Serg" surname="Cherkasov" link="http://facebook.com" />
+        <SayFulName name="Vika" surname="Malovana" link="#" />
+        <SayFulName name="Katya" surname="Cherkasova" link="#" />
+      </div>
+    );
+  }
+}
+
+function Hello() {
+  let phrase = "World!"
+  return (
+      <h1> Hello {phrase} </h1>
+    )
+}
+
+function SayFulName(props) {
+  return (
+    <div>
+    <h1> Моё имя {props.name}, фамилия - {props.surname}</h1>
+    <a href={props.link}> Ссылка на мой профиль </a>
+    </div>
+  )
+}
+
+
+export default App;
